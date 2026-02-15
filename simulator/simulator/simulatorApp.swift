c7da8a85b61b360126d3f32be4263b55a61d2ae2
JarneDM <jarne.demeyer05@hotmail.com>
2026-02-15 16:44:02 +0100

    Add initial project files and structure for simulator app

---
+++ simulator/simulator/simulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  simulatorApp.swift
+//  simulator
+//
+//  Created by Jarne De Meyer on 15/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct simulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
