fd80cd15c9b23e9e603351e252451be02b78ed01
JarneDM <jarne.demeyer05@hotmail.com>
2026-02-24 22:52:51 +0100

    update background and text colors in Index component for improved readability

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,9 @@ import "./index.css";
 
 export default function Index() {
   return (
-    <View className="bg-orange-400/70 h-full flex items-center justify-center">
-      <Text className="text-white text-2xl font-medium">
-        Welcome to <Text className="text-green-300 font-bold">WeeklyMeals!</Text>
+    <View className="bg-slate-200/50 h-full flex items-center justify-center">
+      <Text className="text-black text-2xl font-medium">
+        Welcome to <Text className="text-orange-400 font-bold">WeeklyMeals!</Text>
       </Text>
       <View className="flex flex-row justify-between w-48">
         <Link href="/(auth)/login" className="mt-4 px-4 py-2 bg-white rounded">
